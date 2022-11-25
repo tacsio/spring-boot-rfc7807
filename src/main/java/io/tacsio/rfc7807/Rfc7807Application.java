3ecd7af41b52f5e1d9626a4a86cfcb4384cd66ac
Tarcisio Coutinho <tcs5cin@gmail.com>
2022-11-25 15:15:55 -0300

    initial commit

---
+++ src/main/java/io/tacsio/rfc7807/Rfc7807Application.java
@@ -0,0 +1,13 @@
+package io.tacsio.rfc7807;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rfc7807Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rfc7807Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/tacsio/rfc7807/Rfc7807ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tacsio.rfc7807;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rfc7807ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
